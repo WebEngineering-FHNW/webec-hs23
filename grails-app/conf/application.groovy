2c5f33db2c402b71e6c3057c67149b6bb370165c
Dierk Koenig <dierk.koenig@canoo.com>
2023-11-14 21:11:53 +0100

    security setting with intercept url map

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -29,3 +29,24 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
+
+// config types are 'Annotation', 'Requestmap', or 'InterceptUrlMap'
+ grails.plugin.springsecurity.securityConfigType = 'InterceptUrlMap'
+ grails.plugin.springsecurity.interceptUrlMap =  [
+ 	[pattern: '/static/**',      access: ['permitAll']],
+ 	[pattern: '/',               access: ['permitAll']],
+	[pattern: '/error',          access: ['permitAll']],
+	[pattern: '/index',          access: ['permitAll']],
+	[pattern: '/index.gsp',      access: ['permitAll']],
+	[pattern: '/shutdown',       access: ['permitAll']],
+	[pattern: '/assets/**',      access: ['permitAll']],
+	[pattern: '/**/js/**',       access: ['permitAll']],
+	[pattern: '/**/css/**',      access: ['permitAll']],
+	[pattern: '/**/images/**',   access: ['permitAll']],
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+ 	[pattern: "/login/auth",     access: ["permitAll"]],
+ 	[pattern: "/person/**" ,     access: ['ROLE_ADMIN']], // cannot use constant here :-(
+ 	[pattern: "/room/**"   ,     access: ['ROLE_ADMIN']],
+ 	[pattern: "/**"        ,     access: ['ROLE_ADMIN', 'ROLE_GUEST']],
+ ]
+
